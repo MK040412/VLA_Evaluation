e9ceff6a508871117afd97d86a9b74aa47bfae13
linkurz <linkurzlinkus@gmail.com>
2025-11-04 21:16:33 +0900

    [TITLE] minor update
    [TEST] NA
    [Desc]
    1. commit msg template file to hidden
    2. typo update in run.sh

--- run.sh
+++ run.sh
@@ -30,7 +30,7 @@ if [ ! -d "pretrained_models/rdt" ]; then
     echo "[INFO] rdt pretrained model not found. Downloading..."
     python -m src.utility.download_hf_model robotics-diffusion-transformer/maniskill-model ./pretrained_models rdt/
 fi
-if [ ! -d "pretrained_models/dissusion_policy" ]; then
+if [ ! -d "pretrained_models/diffusion_policy" ]; then
     echo "[INFO] diffusion_policy pretrained model not found. Downloading..."
     python -m src.utility.download_hf_model robotics-diffusion-transformer/maniskill-model ./pretrained_models diffusion_policy/
 fi
@@ -47,10 +47,10 @@ export PYTHONPATH=$(pwd)/src/evaluation
 
 # Evaluate the model on ManiSkill tasks with live view
 python -m src.evaluation.eval_rdt --env-id $ENV_ID --pretrained_path pretrained_models/rdt/mp_rank_00_model_states.pt --lang_embeddings_path $LANG_EMBEDS_PATH --live-view
-python -m src.evaluation.eval_dp --env-id $ENV_ID --pretrained_path pretrained_models/diffusion_policy/700.ckpt
+python -m src.evaluation.eval_dp --env-id $ENV_ID --pretrained_path pretrained_models/diffusion_policy/700.ckpt --vis --save-video
 
-# in progress
-python -m src.evaluation.eval_openvla --env-id $ENV_ID --pretrained_path pretrained_models/openvla
+# # in progress
+# python -m src.evaluation.eval_openvla --env-id $ENV_ID --pretrained_path pretrained_models/openvla
 
-export PYTHONPATH=$(pwd)/src/evaluation/octo
-python -m src.evaluation.eval_octo --env-id $ENV_ID --pretrained_path pretrained_models/octo/experiment_20241208_112612
+# export PYTHONPATH=$(pwd)/src/evaluation/octo
+# python -m src.evaluation.eval_octo --env-id $ENV_ID --pretrained_path pretrained_models/octo/experiment_20241208_112612
